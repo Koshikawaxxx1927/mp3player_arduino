c816c69db42825fedc24fdaa2a3c3e6b15d04990
Koshikawaxxx1927 <ebi1927@icloud.com>
2023-01-15 14:48:34 +0900

    LED lights were working properly.

--- change_bgm/Pin.hpp
+++ change_bgm/Pin.hpp
@@ -2,7 +2,15 @@
 
 namespace pin {
     // DF Player用のRXピン(受信)
-    constexpr uint8_t rx = 10;
+    constexpr uint8_t rx = 3;
     // DF Player用のTXピン(送信)
-    constexpr uint8_t tx = 11;
+    constexpr uint8_t tx = 5;
+
+    // LEDライトのピン
+    constexpr uint8_t first_led = 6; // 1曲目
+    constexpr uint8_t second_led = 7; // 2曲目
+    constexpr uint8_t third_led = 8; // 3曲目
+    constexpr uint8_t fourth_led = 9; // 4曲目
+    // 音量に合わせて光るLEDのピン
+    constexpr uint8_t volume_led = 10;
 };
--- change_bgm/change_bgm.ino
+++ change_bgm/change_bgm.ino
@@ -3,18 +3,26 @@
 
 DFPlayer* myDFPlayer;
 Message msg;
-constexpr uint8_t mp3_num = 4; // SDカードの中に入っている曲の数
+// SDカードの中に入っている曲の数
+// 曲数を変えたら回路も変えなければならない
+constexpr uint8_t mp3_num = 4;
 
 void setup() {
     Serial.begin(9600);
+    // LEDの初期化
+    initialize_led();
+
     myDFPlayer = new DFPlayer();
     msg.dfplayer_type = DFPlayerType::Play;
     msg.mp3 = 1;
+    msg.volume = 20;
     myDFPlayer->update(msg);
+    led_light(msg); // LEDを光らせる
 }
 
 void loop() {
   read_update_msg(msg);
+  led_light(msg); // LEDを光らせる
   //   delay(9000);
   //   msg.dfplayer_type = DFPlayerType::Next;
   //   myDFPlayer->update(msg);
